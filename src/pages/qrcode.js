e4c907061680176c94461b425c7b29ff2b3cdef4
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-04 14:17:28 +0200

    :sparkles: start qrcode provider

---
+++ src/pages/qrcode.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import MainFormQrcode from "../components/MainFormQrcode"
+
+function Qrcode () {
+
+  return (
+    <Layout>
+      <SEO
+        title="Home"
+        keywords = {
+          [
+            `gatsby`, 
+            `tailwind`, 
+            `react`, 
+            `tailwindcss`, 
+            `url`, 
+            `shortener`
+          ]
+        }
+      />
+      <MainFormQrcode/>
+    </Layout>
+  )
+}
+
+export default Qrcode
